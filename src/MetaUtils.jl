b9d1926e00f76c1ae706b4db0162fa3d77cb5425
genkuroki <genkuroki@gmail.com>
2020-10-11 18:03:49 +0900

    Revise show_expr
    Change the showing styles of QuoteNode and LineNumberNode

--- src/MetaUtils.jl
+++ src/MetaUtils.jl
@@ -80,6 +80,13 @@ AbstractRange
 print_tree
 
 """
+    const expr_indent = 4
+
+is the default indent of showing expressions.
+"""
+const expr_indent = 4
+
+"""
     show_expr([io::IO,], ex)
 
 shows expression `ex` as a Julia style expression.
@@ -91,22 +98,23 @@ Expr(:call, :f, :x,
     Expr(:call, :g, :y, :z))
 ```
 """
-show_expr(ex; indent=expr_indent, head="Expr") =
-    show_expr(stdout, ex; indent, head)
-show_expr(io::IO, ex; indent=expr_indent, head="Expr") =
-    show_expr(io, ex, 0; indent, head)
-show_expr(io::IO, ex, inner; indent=expr_indent, head="Expr") =
-    show(io, ex)
+show_expr(io::IO, ex, inner; indent=expr_indent, head="Expr") = show(io, ex)
+show_expr(io::IO, ex; indent=expr_indent, head="Expr") = show_expr(io, ex, 0; indent, head)
+show_expr(ex; indent=expr_indent, head="Expr") = show_expr(stdout, ex; indent, head)
 
-const expr_indent = 4
+function show_expr(io::IO, ex::LineNumberNode, inner; indent=expr_indent, head="Expr")
+    print(io, "LineNumberNode(")
+    show(io, ex.line); print(io, ", ")
+    show(io, ex.file); print(io, ')')
+end
 
 function show_expr(io::IO, ex::QuoteNode, inner; indent=expr_indent, head="Expr")
-    print(io, "(:quote, ")
-    show_expr(io, ex.value, inner+indent; indent)
-    print(io, ')')
+    print(io, "QuoteNode(")
+    show_expr(io, ex.value, inner+indent; indent, head); print(io, ')')
 end
+
 function show_expr(io::IO, ex::Expr, inner; indent=expr_indent, head="Expr")
-    print(io, (iszero(inner) ? "" : "\n"), " "^inner, "$head(")
+    print(io, (iszero(inner) ? "" : "\n"), " "^inner, head, "(")
     show_expr(io, ex.head, inner+indent; indent, head)
     for arg in ex.args
         print(io, ", ")
