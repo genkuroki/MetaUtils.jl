71fb7ec0d3cb59e3ee87b3ac7413bcf206f6a744
genkuroki <genkuroki@gmail.com>
2020-10-17 06:03:07 +0900

    add simple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using MetaUtils
 using Test
 
 @testset "MetaUtils.jl" begin
-    # Write your tests here.
+    @test (:call, :+, (:call, :*, 2, :x), 1) |> texpr2expr == :(2x + 1)
+    @test (:+, (:*, 2, :x), 1) |> texpr2expr == :(2x + 1)
+    @test sprint(show_expr, :(2x + 1)) == 
+        "Expr(:call, :+, \n    Expr(:call, :*, 2, :x), 1)"
+    @test sprint(show_texpr, :(2x + 1)) == 
+        "(:call, :+, \n    (:call, :*, 2, :x), 1)"
 end
