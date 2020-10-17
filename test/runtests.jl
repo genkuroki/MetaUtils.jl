0a781a851328946b4c154d2160bbb0e4a27e1aa5
genkuroki <genkuroki@gmail.com>
2020-10-17 09:19:33 +0900

    add tests of @teval and print_tree

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,15 @@
 using MetaUtils
 using Test
 
+x = 10
+
 @testset "MetaUtils.jl" begin
-    @test (:call, :+, (:call, :*, 2, :x), 1) |> texpr2expr == :(2x + 1)
-    @test (:+, (:*, 2, :x), 1) |> texpr2expr == :(2x + 1)
+    @test (@teval (:call, :+, (:call, :*, 2, :x), 1)) == 21
+    @test (@teval (:+, (:*, 2, :x), 1)) == 21
     @test sprint(show_expr, :(2x + 1)) == 
         "Expr(:call, :+, \n    Expr(:call, :*, 2, :x), 1)"
     @test sprint(show_texpr, :(2x + 1)) == 
         "(:call, :+, \n    (:call, :*, 2, :x), 1)"
+    @test sprint(print_tree, AbstractFloat) ==
+        "AbstractFloat\n├─ BigFloat\n├─ Float16\n├─ Float32\n└─ Float64\n"
 end
